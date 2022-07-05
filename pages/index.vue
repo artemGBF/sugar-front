5a3fa46cb6efddae871778d8731a12745d85b8ff
Шипилов Артём Вадимович <shipilovav@dev.vtb.ru>
2022-07-05 21:55:23 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NavigationBar/>
+  </div>
+</template>
+
+<script>
+import NavigationBar from '../components/NavigationBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default {
+  components: {
+    NavigationBar
+  },
+  data () {
+    return {
+      test: 'test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  display: none;
+}
+</style>
